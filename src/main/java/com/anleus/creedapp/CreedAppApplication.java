d60616afd61b04baa575fe86900c8838cb8b9f4c
Tomás <tomas.rui598@gmail.com>
2024-01-16 12:46:12 +0100

    project initalized

---
+++ src/main/java/com/anleus/creedapp/CreedAppApplication.java
@@ -0,0 +1,13 @@
+package com.anleus.creedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anleus/creedapp/CreedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anleus.creedapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
